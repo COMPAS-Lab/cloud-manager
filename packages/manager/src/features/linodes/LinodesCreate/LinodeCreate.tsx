71280ac5368c25a20f9a015f21c9e43de93d2262
Lawrence Lin <35390215+law-lin@users.noreply.github.com>
2022-10-30 23:11:47 -0400

    Add marketplace

--- packages/manager/src/features/linodes/LinodesCreate/LinodeCreate.tsx
+++ packages/manager/src/features/linodes/LinodesCreate/LinodeCreate.tsx
@@ -44,7 +44,7 @@ import { getErrorMap } from 'src/utilities/errorUtils';
 import { filterCurrentTypes } from 'src/utilities/filterCurrentLinodeTypes';
 import { getParamsFromUrl } from 'src/utilities/queryParams';
 import SelectPlanPanel from './SelectPlanPanel';
-// import FromAppsContent from './TabbedContent/FromAppsContent';
+import FromAppsContent from './TabbedContent/FromAppsContent';
 // import FromBackupsContent from './TabbedContent/FromBackupsContent';
 import FromImageContent from './TabbedContent/FromImageContent';
 // import FromLinodeContent from './TabbedContent/FromLinodeContent';
@@ -276,11 +276,11 @@ export class LinodeCreate extends React.PureComponent<
       type: 'fromImage',
       routeName: `${this.props.match.url}?type=Distributions`,
     },
-    // {
-    //   title: 'Marketplace',
-    //   type: 'fromApp',
-    //   routeName: `${this.props.match.url}?type=One-Click`,
-    // },
+    {
+      title: 'Marketplace',
+      type: 'fromApp',
+      routeName: `${this.props.match.url}?type=One-Click`,
+    },
     {
       title: 'StackScripts',
       type: 'fromStackScript',
@@ -532,7 +532,7 @@ export class LinodeCreate extends React.PureComponent<
                   {...rest}
                 />
               </SafeTabPanel>
-              {/* <SafeTabPanel index={1}>
+              <SafeTabPanel index={1}>
                 <FromAppsContent
                   imagesData={imagesData!}
                   regionsData={regionsData!}
@@ -543,7 +543,7 @@ export class LinodeCreate extends React.PureComponent<
                   errors={errors}
                   {...rest}
                 />
-              </SafeTabPanel> */}
+              </SafeTabPanel>
               <SafeTabPanel index={1}>
                 <Tabs defaultIndex={stackScriptSelectedTab}>
                   <Paper className={classes.stackScriptWrapper}>
