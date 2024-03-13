8c72e870d79f859c5ad994c2b23fc4335333b492
Bindu Bhargava Reddy Chintam <bindubhargavareddy@gmail.com>
2024-03-12 22:47:14 -0400

    Added features to create volume types

--- packages/manager/src/features/Volumes/VolumeCreate/CreateVolumeForm.tsx
+++ packages/manager/src/features/Volumes/VolumeCreate/CreateVolumeForm.tsx
@@ -167,7 +167,14 @@ const CreateVolumeForm: React.FC<CombinedProps> = (props) => {
         values,
         { resetForm, setSubmitting, setStatus, setErrors }
       ) => {
-        const { label, size, region, linode_id, config_id } = values;
+        const {
+          label,
+          size,
+          region,
+          linode_id,
+          config_id,
+          volume_type,
+        } = values;
 
         setSubmitting(true);
 
@@ -190,6 +197,7 @@ const CreateVolumeForm: React.FC<CombinedProps> = (props) => {
             config_id === initialValueDefaultId
               ? undefined
               : maybeCastToNumber(config_id),
+          hardware_type: volume_type == 'nvme' ? 'ssd_volume' : 'hdd_volume',
         })
           .then(({ filesystem_path, label: volumeLabel }) => {
             if (hasSignedAgreement) {
