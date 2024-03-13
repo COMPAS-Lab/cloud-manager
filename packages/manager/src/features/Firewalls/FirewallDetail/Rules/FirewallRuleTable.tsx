af3caac66c7ee2564bec0ab10855bab8cb6a83a9
AlanH60 <85328997+AlanH60@users.noreply.github.com>
2024-03-12 22:37:47 -0400

    Removed Draggable Rows in Firwall Table
    OpenStack security group rules can't be given any order

--- packages/manager/src/features/Firewalls/FirewallDetail/Rules/FirewallRuleTable.tsx
+++ packages/manager/src/features/Firewalls/FirewallDetail/Rules/FirewallRuleTable.tsx
@@ -320,6 +320,9 @@ const FirewallRuleTable: React.FC<CombinedProps> = (props) => {
                         key={thisRuleRow.id}
                         draggableId={String(thisRuleRow.id)}
                         index={index}
+                        /* -- Clanode Change -- */
+                        isDragDisabled={true}
+                        /* -- Clanode Change End -- */
                       >
                         {(provided) => (
                           <li
