5ccc30b48d366f0b36f39d54809c0ec6735ee1f8
AlanH60 <85328997+AlanH60@users.noreply.github.com>
2024-03-15 13:42:13 -0400

    Added Volume Price Display to CreateVolumeForLinodeForm

--- packages/manager/src/queries/volumeTypes.ts
+++ packages/manager/src/queries/volumeTypes.ts
@@ -4,11 +4,17 @@ import { useQuery } from 'react-query';
 import { queryClient, queryPresets } from './base';
 import { defaultVolumeTypes } from '../constants';
 
+export const volumeQueryOptions = {
+  ...queryPresets.longLived,
+  placeholderData: defaultVolumeTypes,
+  onError: () => {
+    queryClient.setQueryData('volume_types', defaultVolumeTypes);
+  },
+};
+
 export const useVolumeTypesQuery = () =>
-  useQuery<VolumeType[], APIError[]>('volume_types', getVolumeTypes, {
-    ...queryPresets.longLived,
-    placeholderData: defaultVolumeTypes,
-    onError: () => {
-      queryClient.setQueryData('volume_types', []);
-    },
-  });
+  useQuery<VolumeType[], APIError[]>(
+    'volume_types',
+    getVolumeTypes,
+    volumeQueryOptions
+  );
