302469e37a69899efcfc6d59051461195176aee7
AlanH60 <85328997+AlanH60@users.noreply.github.com>
2024-03-15 12:16:03 -0400

    Add Untracked Files

---
+++ packages/manager/src/queries/volumeTypes.ts
@@ -0,0 +1,14 @@
+import { getVolumeTypes, VolumeType } from '@linode/api-v4/lib/volumes';
+import { APIError } from '@linode/api-v4/lib/types';
+import { useQuery } from 'react-query';
+import { queryClient, queryPresets } from './base';
+import { defaultVolumeTypes } from '../constants';
+
+export const useVolumeTypesQuery = () =>
+  useQuery<VolumeType[], APIError[]>('volume_types', getVolumeTypes, {
+    ...queryPresets.longLived,
+    placeholderData: defaultVolumeTypes,
+    onError: () => {
+      queryClient.setQueryData('volume_types', []);
+    },
+  });
