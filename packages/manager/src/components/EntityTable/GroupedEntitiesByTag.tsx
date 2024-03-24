1f64a55ae78e9e20abe7c1cf1bc7f0ac34ddaaaf
AlanH60 <85328997+AlanH60@users.noreply.github.com>
2024-03-24 11:49:19 -0400

    Fixed Issue with Spacing in Volume Tagged View
    The tag header did not have a left margin so it looked awkward, especially in dark mode

--- packages/manager/src/components/EntityTable/GroupedEntitiesByTag.tsx
+++ packages/manager/src/components/EntityTable/GroupedEntitiesByTag.tsx
@@ -27,6 +27,11 @@ const useStyles = makeStyles((theme: Theme) => ({
       borderTop: 'none',
     },
   },
+  /* -- Clanode Change -- */
+  td: {
+    paddingLeft: theme.spacing(),
+  },
+  /* -- Clanode Change End -- */
   groupContainer: {
     [theme.breakpoints.up('md')]: {
       '& $tagHeaderRow > td': {
@@ -37,6 +42,9 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
   tagHeader: {
     marginBottom: 2,
+    /* -- Clanode Change -- */
+    marginLeft: theme.spacing(),
+    /* -- Clanode Change End -- */
   },
   paginationCell: {
     paddingTop: 2,
@@ -114,6 +122,7 @@ export const GroupedEntitiesByTag: React.FC<CombinedProps> = (props) => {
                           </TableRow>
                           {paginatedData.map((thisEntity) => (
                             <RowComponent
+                              className={classes.td}
                               key={thisEntity.id}
                               {...thisEntity}
                               {...handlers}
