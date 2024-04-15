09fc0fd4efff35d2c99accf43f1d1aa1a2455241
AlanH60 <85328997+AlanH60@users.noreply.github.com>
2024-04-15 02:51:03 -0400

    Event Handling to Update Firewalls in Linode Details Page

--- packages/manager/src/store/firewalls/firewalls.events.ts
+++ packages/manager/src/store/firewalls/firewalls.events.ts
@@ -1,40 +1,33 @@
 import { EventStatus } from '@linode/api-v4/lib/account';
+import {
+  FirewallDevice,
+  getFirewallDevices,
+} from '@linode/api-v4/lib/firewalls';
+import { ResourcePage } from '@linode/api-v4/lib/types';
 import { Dispatch } from 'redux';
 import { EventHandler } from 'src/store/types';
 import { getAllFirewalls } from './firewalls.requests';
 import { getAllFirewallDevices } from './devices.requests';
+import { refreshLinodeFirewalls } from 'src/queries/linodeFirewalls';
 
 const firewallEventsHandler: EventHandler = (event, dispatch) => {
-  const { action, status, entity } = event;
+  const { action, status, entity, secondary_entity } = event;
   const { id } = entity;
+  const deviceId = secondary_entity?.id;
 
   switch (action) {
     case 'firewall_update':
-      return handleFirewallUpdate(dispatch, status);
+      return handleFirewallUpdate(dispatch, status, id);
     case 'firewall_device_add':
     case 'firewall_device_remove':
-      return handleFirewallDeviceUpdate(dispatch, status, id);
+      return handleFirewallDeviceUpdate(dispatch, status, id, deviceId);
 
     default:
       return;
   }
 };
 
-const handleFirewallUpdate = (dispatch: Dispatch<any>, status: EventStatus) => {
-  switch (status) {
-    case 'failed':
-    case 'finished':
-    case 'notification':
-    case 'scheduled':
-    case 'started':
-      dispatch(getAllFirewalls({}));
-
-    default:
-      return;
-  }
-};
-
-const handleFirewallDeviceUpdate = (
+const handleFirewallUpdate = (
   dispatch: Dispatch<any>,
   status: EventStatus,
   firewallID: number
@@ -45,6 +38,35 @@ const handleFirewallDeviceUpdate = (
     case 'notification':
     case 'scheduled':
     case 'started':
+      dispatch(getAllFirewalls({}));
+      getFirewallDevices(firewallID).then(
+        (res: ResourcePage<FirewallDevice>) => {
+          res.data.forEach((device: FirewallDevice) =>
+            refreshLinodeFirewalls(device.id)
+          );
+          return res;
+        }
+      );
+
+    default:
+      return;
+  }
+};
+
+const handleFirewallDeviceUpdate = (
+  dispatch: Dispatch<any>,
+  status: EventStatus,
+  firewallID: number,
+  deviceID?: number
+) => {
+  switch (status) {
+    case 'failed':
+    case 'finished':
+    case 'notification':
+    case 'scheduled':
+    case 'started':
+      //Force update Linode Firewalls if deviceID is given
+      if (deviceID) refreshLinodeFirewalls(deviceID);
       dispatch(getAllFirewallDevices({ firewallID }));
 
     default:
