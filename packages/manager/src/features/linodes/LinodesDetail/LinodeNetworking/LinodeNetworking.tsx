linode/manager

UI updates, fix auth flow, use instance snapshots instead of volume snapshots, confirm resizing child process

packages/manager/src/features/linodes/LinodesDetail/LinodeNetworking/LinodeNetworking.tsx
@@ -10,7 +10,7 @@ import * as React from 'react';
 import { connect, MapDispatchToProps } from 'react-redux';
 import { compose as recompose } from 'recompose';
 import AddNewLink from 'src/components/AddNewLink';
-import Button from 'src/components/Button';
+// import Button from 'src/components/Button';
 import CircleProgress from 'src/components/CircleProgress';
 import Hidden from 'src/components/core/Hidden';
 import Paper from 'src/components/core/Paper';
@@ -43,8 +43,8 @@ import DeleteIPConfirm from './DeleteIPConfirm';
 import EditRDNSDrawer from './EditRDNSDrawer';
 import IPSharing from './IPSharing';
 import IPTransfer from './IPTransfer';
-import LinodeNetworkingActionMenu from './LinodeNetworkingActionMenu';
-import LinodeNetworkingSummaryPanel from './NetworkingSummaryPanel';
+// import LinodeNetworkingActionMenu from './LinodeNetworkingActionMenu';
+// import LinodeNetworkingSummaryPanel from './NetworkingSummaryPanel';
 import { IPTypes } from './types';
 import ViewIPDrawer from './ViewIPDrawer';
 import ViewRangeDrawer from './ViewRangeDrawer';
@@ -290,8 +290,8 @@ class LinodeNetworking extends React.Component<CombinedProps, State> {
   };
 
   renderIPRow = (ipDisplay: IPDisplay) => {
-    const { classes, readOnly } = this.props;
-    const { address, type, gateway, subnetMask, rdns, _ip, _range } = ipDisplay;
+    // const { classes, readOnly } = this.props;
+    const { address, type } = ipDisplay;
 
     return (
       <TableRow key={`${address}-${type}`} data-qa-ip={address}>
@@ -301,13 +301,13 @@ class LinodeNetworking extends React.Component<CombinedProps, State> {
         <TableCell parentColumn="Type" data-qa-ip-address>
           {type}
         </TableCell>
-        <TableCell parentColumn="Default Gateway">{gateway}</TableCell>
-        <TableCell parentColumn="Subnet Mask">{subnetMask}</TableCell>
-        <TableCell parentColumn="Reverse DNS" data-qa-rdns>
-          {/* Ranges have special handling for RDNS. */}
-          {_range ? this.renderRangeRDNSCell(_range) : rdns}
-        </TableCell>
-        <TableCell className={classes.action} data-qa-action>
+        {/* <TableCell parentColumn="Default Gateway">{gateway}</TableCell>
+        <TableCell parentColumn="Subnet Mask">{subnetMask}</TableCell> */}
+        {/* <TableCell parentColumn="Reverse DNS" data-qa-rdns> */}
+        {/* Ranges have special handling for RDNS. */}
+        {/* {_range ? this.renderRangeRDNSCell(_range) : rdns} */}
+        {/* </TableCell> */}
+        {/* <TableCell className={classes.action} data-qa-action>
           {_ip ? (
             <LinodeNetworkingActionMenu
               onEdit={this.handleOpenEditRDNS}
@@ -325,7 +325,7 @@ class LinodeNetworking extends React.Component<CombinedProps, State> {
               readOnly={readOnly}
             />
           ) : null}
-        </TableCell>
+        </TableCell> */}
       </TableRow>
     );
   };
@@ -424,9 +424,9 @@ class LinodeNetworking extends React.Component<CombinedProps, State> {
       readOnly,
       linode: {
         id: linodeID,
-        label: linodeLabel,
+        // label: linodeLabel,
         region: linodeRegion,
-        created: linodeCreated,
+        // created: linodeCreated,
       },
     } = this.props;
     const {
@@ -489,12 +489,12 @@ class LinodeNetworking extends React.Component<CombinedProps, State> {
     return (
       <div>
         {readOnly && <LinodePermissionsError />}
-        <LinodeNetworkingSummaryPanel
+        {/* <LinodeNetworkingSummaryPanel
           linodeRegion={linodeRegion}
           linodeID={linodeID}
           linodeCreated={linodeCreated}
           linodeLabel={linodeLabel}
-        />
+        /> */}
 
         {this.renderIPTable()}
 
@@ -609,7 +609,7 @@ class LinodeNetworking extends React.Component<CombinedProps, State> {
           body={
             <Hidden mdUp>
               <div style={{ padding: 5 }}>
-                <Hidden xsDown>
+                {/* <Hidden xsDown>
                   <Button
                     onClick={this.openTransferDialog}
                     buttonType="secondary"
@@ -623,7 +623,7 @@ class LinodeNetworking extends React.Component<CombinedProps, State> {
                   >
                     IP Sharing
                   </Button>
-                </Hidden>
+                </Hidden> */}
                 <AddNewLink
                   label="Add an IP Address"
                   onClick={this.openAddIPDrawer}
@@ -634,7 +634,7 @@ class LinodeNetworking extends React.Component<CombinedProps, State> {
           actions={
             <Hidden smDown>
               <div style={{ padding: 5 }}>
-                <Button
+                {/* <Button
                   onClick={this.openTransferDialog}
                   buttonType="secondary"
                 >
@@ -646,7 +646,7 @@ class LinodeNetworking extends React.Component<CombinedProps, State> {
                   buttonType="secondary"
                 >
                   IP Sharing
-                </Button>
+                </Button> */}
                 <AddNewLink
                   label="Add an IP Address"
                   onClick={this.openAddIPDrawer}
@@ -673,7 +673,7 @@ class LinodeNetworking extends React.Component<CombinedProps, State> {
                       >
                         Type
                       </TableSortCell>
-                      <TableCell style={{ width: '10%' }}>
+                      {/* <TableCell style={{ width: '10%' }}>
                         Default Gateway
                       </TableCell>
                       <TableCell style={{ width: '10%' }}>
@@ -681,8 +681,8 @@ class LinodeNetworking extends React.Component<CombinedProps, State> {
                       </TableCell>
                       <TableCell style={{ width: '20%', borderRight: 'none' }}>
                         Reverse DNS
-                      </TableCell>
-                      <TableCell style={{ width: '20%', borderLeft: 'none' }} />
+                      </TableCell> */}
+                      {/* <TableCell style={{ width: '20%', borderLeft: 'none' }} /> */}
                     </TableRow>
                   </TableHead>
                   <TableBody>{orderedData.map(this.renderIPRow)}</TableBody>

packages/manager/src/features/linodes/LinodesLanding/LinodeRow/LinodeRow.tsx
@@ -35,7 +35,7 @@ import { parseMaintenanceStartTime } from '../utils';
 import withNotifications, { WithNotifications } from '../withNotifications';
 import withRecentEvent, { WithRecentEvent } from '../withRecentEvent';
 import styled, { StyleProps } from './LinodeRow.style';
-import LinodeRowBackupCell from './LinodeRowBackupCell';
+// import LinodeRowBackupCell from './LinodeRowBackupCell';
 import LinodeRowHeadCell from './LinodeRowHeadCell';
 
 interface Props {
@@ -109,7 +109,7 @@ export const LinodeRow: React.FC<CombinedProps> = (props) => {
     mutationAvailable,
   } = props;
 
-  const isBareMetalInstance = type?.class === 'metal';
+  // const isBareMetalInstance = type?.class === 'metal';
 
   const loading = linodeInTransition(status, recentEvent);
   const parsedMaintenanceStartTime = parseMaintenanceStartTime(
@@ -224,14 +224,14 @@ export const LinodeRow: React.FC<CombinedProps> = (props) => {
           </TableCell>
         </Hidden>
       </Hidden>
-      <Hidden mdDown>
+      {/* <Hidden mdDown>
         <LinodeRowBackupCell
           linodeId={id}
           backupsEnabled={backups.enabled || false}
           mostRecentBackup={mostRecentBackup || ''}
           isBareMetalInstance={isBareMetalInstance}
         />
-      </Hidden>
+      </Hidden> */}
 
       <TableCell actionCell data-qa-notifications>
         <RenderFlag

packages/manager/src/features/linodes/LinodesLanding/SortableTableHead.tsx
@@ -167,7 +167,7 @@ const SortableTableHead: React.FC<CombinedProps> = (props) => {
                 </TableSortCell>
               </Hidden>
             </Hidden>
-            <Hidden mdDown>
+            {/* <Hidden mdDown>
               <TableSortCell
                 noWrap
                 label="backups:last_successful"
@@ -178,7 +178,7 @@ const SortableTableHead: React.FC<CombinedProps> = (props) => {
               >
                 Last Backup
               </TableSortCell>
-            </Hidden>
+            </Hidden> */}
           </>
         )}
         <TableCell>

COMPAS-Lab/cloud-manager

packages/manager/src/features/linodes/LinodesDetail/LinodeNetworking/LinodeNetworking.tsx
@@ -17,7 +17,7 @@ import * as React from 'react';
 import { connect, MapDispatchToProps } from 'react-redux';
 import { compose as recompose } from 'recompose';
 import AddNewLink from 'src/components/AddNewLink';
-import Button from 'src/components/Button';
+// import Button from 'src/components/Button';
 import CircleProgress from 'src/components/CircleProgress';
 import CopyTooltip from 'src/components/CopyTooltip';
 import Hidden from 'src/components/core/Hidden';
@@ -51,8 +51,8 @@ import DeleteIPConfirm from './DeleteIPConfirm';
 import EditRDNSDrawer from './EditRDNSDrawer';
 import IPSharing from './IPSharing';
 import IPTransfer from './IPTransfer';
-import LinodeNetworkingActionMenu from './LinodeNetworkingActionMenu';
-import LinodeNetworkingSummaryPanel from './NetworkingSummaryPanel';
+// import LinodeNetworkingActionMenu from './LinodeNetworkingActionMenu';
+// import LinodeNetworkingSummaryPanel from './NetworkingSummaryPanel';
 import { IPTypes } from './types';
 import ViewIPDrawer from './ViewIPDrawer';
 import ViewRangeDrawer from './ViewRangeDrawer';
@@ -381,8 +381,8 @@ class LinodeNetworking extends React.Component<CombinedProps, State> {
   };
 
   renderIPRow = (ipDisplay: IPDisplay) => {
-    const { classes, readOnly } = this.props;
-    const { address, type, gateway, subnetMask, rdns, _ip, _range } = ipDisplay;
+    // const { classes, readOnly } = this.props;
+    const { address, type } = ipDisplay;
 
     return (
       <TableRow
@@ -401,13 +401,13 @@ class LinodeNetworking extends React.Component<CombinedProps, State> {
         <TableCell parentColumn="Type" data-qa-ip-address>
           {type}
         </TableCell>
-        <TableCell parentColumn="Default Gateway">{gateway}</TableCell>
-        <TableCell parentColumn="Subnet Mask">{subnetMask}</TableCell>
-        <TableCell parentColumn="Reverse DNS" data-qa-rdns>
-          {/* Ranges have special handling for RDNS. */}
-          {_range ? this.renderRangeRDNSCell(_range) : rdns}
-        </TableCell>
-        <TableCell className={classes.action} data-qa-action>
+        {/* <TableCell parentColumn="Default Gateway">{gateway}</TableCell>
+        <TableCell parentColumn="Subnet Mask">{subnetMask}</TableCell> */}
+        {/* <TableCell parentColumn="Reverse DNS" data-qa-rdns> */}
+        {/* Ranges have special handling for RDNS. */}
+        {/* {_range ? this.renderRangeRDNSCell(_range) : rdns} */}
+        {/* </TableCell> */}
+        {/* <TableCell className={classes.action} data-qa-action>
           {_ip ? (
             <LinodeNetworkingActionMenu
               onEdit={this.handleOpenEditRDNS}
@@ -425,7 +425,7 @@ class LinodeNetworking extends React.Component<CombinedProps, State> {
               readOnly={readOnly}
             />
           ) : null}
-        </TableCell>
+        </TableCell> */}
       </TableRow>
     );
   };
@@ -525,9 +525,9 @@ class LinodeNetworking extends React.Component<CombinedProps, State> {
       readOnly,
       linode: {
         id: linodeID,
-        label: linodeLabel,
+        // label: linodeLabel,
         region: linodeRegion,
-        created: linodeCreated,
+        // created: linodeCreated,
       },
     } = this.props;
     const {
@@ -587,12 +587,12 @@ class LinodeNetworking extends React.Component<CombinedProps, State> {
     return (
       <div>
         {readOnly && <LinodePermissionsError />}
-        <LinodeNetworkingSummaryPanel
+        {/* <LinodeNetworkingSummaryPanel
           linodeRegion={linodeRegion}
           linodeID={linodeID}
           linodeCreated={linodeCreated}
           linodeLabel={linodeLabel}
-        />
+        /> */}
 
         {this.renderIPTable()}
 
@@ -719,7 +719,7 @@ class LinodeNetworking extends React.Component<CombinedProps, State> {
           body={
             <Hidden mdUp>
               <div style={{ padding: 5 }}>
-                <Hidden smDown>
+                {/* <Hidden smDown>
                   <Button
                     onClick={this.openTransferDialog}
                     buttonType="secondary"
@@ -733,7 +733,7 @@ class LinodeNetworking extends React.Component<CombinedProps, State> {
                   >
                     IP Sharing
                   </Button>
-                </Hidden>
+                </Hidden> */}
                 <AddNewLink
                   label="Add an IP Address"
                   onClick={this.openAddIPDrawer}
@@ -744,7 +744,7 @@ class LinodeNetworking extends React.Component<CombinedProps, State> {
           actions={
             <Hidden mdDown>
               <div style={{ padding: 5 }}>
-                <Button
+                {/* <Button
                   onClick={this.openTransferDialog}
                   buttonType="secondary"
                 >
@@ -756,7 +756,7 @@ class LinodeNetworking extends React.Component<CombinedProps, State> {
                   buttonType="secondary"
                 >
                   IP Sharing
-                </Button>
+                </Button> */}
                 <AddNewLink
                   label="Add an IP Address"
                   onClick={this.openAddIPDrawer}
@@ -783,7 +783,7 @@ class LinodeNetworking extends React.Component<CombinedProps, State> {
                       >
                         Type
                       </TableSortCell>
-                      <TableCell style={{ width: '10%' }}>
+                      {/* <TableCell style={{ width: '10%' }}>
                         Default Gateway
                       </TableCell>
                       <TableCell style={{ width: '10%' }}>
@@ -791,8 +791,8 @@ class LinodeNetworking extends React.Component<CombinedProps, State> {
                       </TableCell>
                       <TableCell style={{ width: '20%', borderRight: 'none' }}>
                         Reverse DNS
-                      </TableCell>
-                      <TableCell style={{ width: '20%', borderLeft: 'none' }} />
+                      </TableCell> */}
+                      {/* <TableCell style={{ width: '20%', borderLeft: 'none' }} /> */}
                     </TableRow>
                   </TableHead>
                   <TableBody>{orderedData.map(this.renderIPRow)}</TableBody>

packages/manager/src/features/linodes/LinodesLanding/LinodeRow/LinodeRow.tsx
@@ -35,7 +35,7 @@ import { parseMaintenanceStartTime } from '../utils';
 import withNotifications, { WithNotifications } from '../withNotifications';
 import withRecentEvent, { WithRecentEvent } from '../withRecentEvent';
 import styled, { StyleProps } from './LinodeRow.style';
-import LinodeRowBackupCell from './LinodeRowBackupCell';
+// import LinodeRowBackupCell from './LinodeRowBackupCell';
 import LinodeRowHeadCell from './LinodeRowHeadCell';
 
 interface Props {
@@ -104,7 +104,7 @@ export const LinodeRow: React.FC<CombinedProps> = (props) => {
     mutationAvailable,
   } = props;
 
-  const isBareMetalInstance = type?.class === 'metal';
+  // const isBareMetalInstance = type?.class === 'metal';
 
   const loading = linodeInTransition(status, recentEvent);
   const parsedMaintenanceStartTime = parseMaintenanceStartTime(
@@ -220,14 +220,14 @@ export const LinodeRow: React.FC<CombinedProps> = (props) => {
           </TableCell>
         </Hidden>
       </Hidden>
-      <Hidden lgDown>
+      {/* <Hidden lgDown>
         <LinodeRowBackupCell
           linodeId={id}
           backupsEnabled={backups.enabled || false}
           mostRecentBackup={mostRecentBackup || ''}
           isBareMetalInstance={isBareMetalInstance}
         />
-      </Hidden>
+      </Hidden> */}
 
       <TableCell actionCell data-qa-notifications>
         <RenderFlag

packages/manager/src/features/linodes/LinodesLanding/SortableTableHead.tsx
@@ -159,7 +159,7 @@ const SortableTableHead: React.FC<CombinedProps> = (props) => {
                 </TableSortCell>
               </Hidden>
             </Hidden>
-            <Hidden lgDown>
+            {/* <Hidden lgDown>
               <TableSortCell
                 noWrap
                 label="backups:last_successful"
@@ -170,7 +170,7 @@ const SortableTableHead: React.FC<CombinedProps> = (props) => {
               >
                 Last Backup
               </TableSortCell>
-            </Hidden>
+            </Hidden> */}
           </>
         )}
         <TableCell>